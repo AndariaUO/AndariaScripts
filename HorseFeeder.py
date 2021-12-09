203a810a0e62f0016632135427b5b7bfe713455a
PoodyCZ <poody27@gmail.com>
2021-12-09 14:12:51 +0100

    +HorseFeeder.py

---
+++ HorseFeeder.py
@@ -0,0 +1,69 @@
+from Assistant import Engine
+from DorchLib import *
+
+pause = 500
+
+def Main():
+	Init()
+	while FindHorse():
+		FeedHorse()
+		Pause(pause)
+		WaterHorse()
+		Pause(pause)
+	SysMessage("Hotovo")
+	
+def Init():
+	Makelist("horses")
+	Makelist("watersources")
+	ClearIgnoreList()
+	if FindType(VratTyp("pripousteci_hul"),-1,"self"):
+		EquipType(VratTyp("pripousteci_hul"), "TwoHanded")
+	else:
+		if not ConfirmPrompt("Nemám připouštěcí hůl, chceš pokračovat?"):
+			Stop()
+	
+def FindHorse():
+	if FindTypeList("horses", 3):
+		SetAlias("horse", "found")
+		IgnoreObject("horse")
+		HeadMsg("Teď já!", "horse")
+		return True
+			
+	return False
+	
+def FeedHorse():
+	print Name("horse")
+	while True:
+		ClearJournal()
+		if not FindType(VratTyp("seno"), -1, "self"):
+			if not FindType(VratTyp("seno"), 3):
+				SysMessage("Nemám seno")
+				return
+		MoveItem("found", "horse", 1)
+		WhisperMsg("all stop")
+		Pause(pause)
+		if InJournal("Asi se to tvému miláèkovi pøíli nelíbí..."):
+			return
+
+
+def WaterHorse():
+	while True:
+		ClearJournal()
+		Msg(".usehand")
+		WaitForTarget(1000)
+		Target("horse")
+		WaitForGump(0x0000042e, 1000)
+		if GumpExists(0x0000042e):
+			ReplyGump(0x0000042e, 1)
+		if not FindTypeList("watersources", 2):
+			SysMessage("Nemám žádný zdroj vody")
+			break
+		Target("found")
+		Pause(pause)
+		if InJournal("nenapije") or InJournal("daleko"):
+			IgnoreObject("found")
+			HeadMsg("Jsem prázdný nebo moc daleko", "found")
+		else:
+			break
+
+Main()
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# DorchScripts
+<h3 align="center">Dorchaidovy skripty pro Classic Assist</h3>
+
+### Požadavky
+
+1. Skripty vyžadují [DorchLib](https://github.com/PoodyCZ/DorchLib), návod k instalaci je tam.
+
+### Instalace
+
+1. Otevři soubor se skriptem v modu raw
+2. Zkopíruj všechen text
+3. V ClassicAssist založ nové makro
+4. Vlož text
