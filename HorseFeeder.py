6c853a7f55df21452b74cdd95d2463f8ae945fe1
PoodyCZ <poody27@gmail.com>
2022-03-05 09:54:38 +0100

    Update

--- HorseFeeder.py
+++ HorseFeeder.py
@@ -2,6 +2,7 @@ from Assistant import Engine
 from DorchLib import *
 
 pause = 500
+gumpID = 0x000003ff # Toto se občas mění z nějakého důvodu, je třeba potom otevřít gump připouštěcí hole, potom otevřít brouka vpravo nahoře v CA a z Gumpů zkopírovat Serial
 
 def Main():
 	Init()
@@ -14,11 +15,12 @@ def Main():
 	
 def Init():
 	ClearIgnoreList()
-	if FindTypeBy("pripousteci_hul",-1,"self"):
-		EquipType(GetGraphicsIdWithType("pripousteci_hul"), "TwoHanded")
+	if FindTypeBy("pripousteci_hul", -1, "self"):
+		EquipType(GetType("pripousteci_hul"), "TwoHanded")
 	else:
 		if not ConfirmPrompt("Nemám připouštěcí hůl, chceš pokračovat?"):
 			Stop()
+	UnsetAlias("found")
 	
 def FindHorse():
 	if FindTypeList("horses", 3):
@@ -49,9 +51,9 @@ def WaterHorse():
 		Msg(".usehand")
 		WaitForTarget(1000)
 		Target("horse")
-		WaitForGump(0x0000042e, 1000)
-		if GumpExists(0x0000042e):
-			ReplyGump(0x0000042e, 1)
+		WaitForGump(gumpID, 3000)
+		if GumpExists(gumpID):
+			ReplyGump(gumpID, 1)
 		if not FindTypeList("watersources", -1, "self"):
 			if not FindTypeList("watersources", 2):
 				SysMessage("Nemám žádný zdroj vody")
